547a425d893b7023a7dd3b926d57fcc39ec1508d
MinsuHan <trms7794han@gmail.com>
2023-06-08 17:07:21 +0900

    이벤트핸들링, useState

--- 03-react-basics-working-with-components/starter/my-app/src/components/Expenses/ExpenseItem.js
+++ 03-react-basics-working-with-components/starter/my-app/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("updated");
+    console.log(title);
+  };
+
   // jsx는 중괄호 안에서 자바스크립트 문법을 실행할 수 있다
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
