f4f289fd82b2c7504e96ff012ae448af535be425
MinsuHan <trms7794han@gmail.com>
2023-06-22 17:39:52 +0900

    useReducer + useContext 상태 관리 관련 주석 추가

--- 11-practice-food-order-app/starter-app/src/store/CartProvider.js
+++ 11-practice-food-order-app/starter-app/src/store/CartProvider.js
@@ -2,18 +2,22 @@ import { useReducer } from "react";
 
 import CartContext from "./cart-context";
 
+// Reducer init state
 const defaultCartState = {
   items: [],
   totalAmount: 0,
 };
 
+// Reducer 함수 (dispatch)
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
+    // 기존 state를 유지하고 새로운 state를 생성
     const updatedItems = state.items.concat(action.item);
 
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
+    // State Update
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -22,12 +26,15 @@ const cartReducer = (state, action) => {
   return defaultCartState;
 };
 
+// Context Provider
 const CartProvider = (props) => {
+  // State (useReducer Hook으로 관리)
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
     defaultCartState
   );
 
+  // context로 제공하는 상태 수정 함수
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: "ADD", item: item });
   };
@@ -36,9 +43,13 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  // 전역적으로 제공할 Context
   const cartContext = {
+    // 상태 읽기
     items: cartState.items,
     totalAmount: cartState.totalAmount,
+
+    // 상태 수정
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
