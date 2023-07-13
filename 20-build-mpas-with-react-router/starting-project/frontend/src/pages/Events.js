6f8fe40162907e838f5852ddeee64710e665adfe
MinsuHan <trms7794han@gmail.com>
2023-07-13 17:25:33 +0900

    커스텀 오류(errorElement), loader params

--- 20-build-mpas-with-react-router/starting-project/frontend/src/pages/Events.js
+++ 20-build-mpas-with-react-router/starting-project/frontend/src/pages/Events.js
@@ -1,9 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData(); // 가장 가까운 데이터에 액세스
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
   const events = data.events;
   return <EventsList events={events} />;
 }
@@ -14,7 +18,11 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     return response;
   }
