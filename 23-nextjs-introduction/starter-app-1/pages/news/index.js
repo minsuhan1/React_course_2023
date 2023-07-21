25d6dca3f6de87f56365dd2c3a387ae50e8bee67
MinsuHan <trms7794han@gmail.com>
2023-07-21 09:38:40 +0900

    동적 페이지, 동적 매개변수 값 추출, 페이지 간 연결 (Link)'

--- 23-nextjs-introduction/starter-app-1/pages/news/index.js
+++ 23-nextjs-introduction/starter-app-1/pages/news/index.js
@@ -1,7 +1,21 @@
 // our-domain.com/news
+import Link from "next/link";
+import { Fragment } from "react";
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
--- 23-nextjs-introduction/starter-app-1/pages/news/something-important.js
+++
@@ -1,7 +0,0 @@
-// our-domain.com/news
-
-function DetailPage() {
-  return <h1>The Detail Page</h1>;
-}
-
-export default DetailPage;
