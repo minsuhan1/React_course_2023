d8cb3cf7072e6ff21497b97d4851026058aa90c7
MinsuHan <trms7794han@gmail.com>
2023-07-24 17:51:45 +0900

    pre-rendering: getStaticProps, getStaticPaths, getServerSideProps, API routing

---
+++ 23-nextjs-introduction/starter-app-2/pages/index.js
@@ -0,0 +1,62 @@
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+import { Fragment } from "react";
+
+function HomePage(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
+  );
+}
+
+// 새로운 요청이 들어올 때마다 서버에서 프리 로드됨
+// 따라서 시간을 지정해서 revalidate 할 필요가 없다
+// export async function getServerSideProps(context) {
+//   const req = context.req; // 요청 객체(인증, 세션쿠키 등 확인 가능)
+//   const res = context.res;
+
+//   // fetch data from an API
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb+srv://trms7794han:eUWrslSKfaQzg1Ro@cluster0.dzy7kay.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
---
+++ 23-nextjs-introduction/starter-app-2/pages/new-meetup/index.js
@@ -0,0 +1,36 @@
+// our-domain.com/new-meetup
+import { useRouter } from "next/router";
+import Head from "next/head";
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import { Fragment } from "react";
+
+function NewMeetupPage() {
+  const router = useRouter();
+  async function addMeetupHandler(enteredMeetupData) {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.push("/");
+  }
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta name="description" content="Add you own meetups" />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+    </Fragment>
+  );
+}
+
+export default NewMeetupPage;
