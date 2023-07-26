dad15f1c24f614977f36955aeb85273f70272326
MinsuHan <trms7794han@gmail.com>
2023-07-26 17:18:46 +0900

    단위테스트: react testing library, jest.fn()

---
+++ 26-testing/starter-app/src/App.js
@@ -0,0 +1,15 @@
+import Greeting from "./components/Greeting";
+import logo from "./logo.svg";
+import "./App.css";
+import Async from "./components/Async";
+
+function App() {
+  return (
+    <div className="App">
+      <Async />
+      <Greeting />
+    </div>
+  );
+}
+
+export default App;
